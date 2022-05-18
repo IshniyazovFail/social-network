496bc9e1fa1d3c28151b88e6ebeea9fa13445ca1
IshniyazovFail <filimen@mail.ru>
2022-05-18 14:38:33 +0300

    added avatars to dialogues

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -10,7 +10,8 @@ export const DialogItem = (p: DialogsItemType) => {
     let path = '/dialogs/' + p.id
     return (
         <div className={`${s.dialog} ${s.active}`}>
-            <NavLink to={path}>{p.name}</NavLink>
+            <img className={s.img} alt='' src='https://coolsen.ru/wp-content/uploads/2021/06/108-8.jpg'/>
+            <NavLink activeClassName={s.active} to={path}>{p.name}</NavLink>
         </div>
     )
 }
