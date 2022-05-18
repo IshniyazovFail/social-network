ea0d2b3ca1f50e3626bef6aefb556b5be5926c3d
IshniyazovFail <filimen@mail.ru>
2022-05-18 15:22:52 +0300

    addded style dialogs

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -1,12 +1,16 @@
 
 import React from "react";
-
+import s from './Message.module.css'
 type MessageType = {
     message: string
 }
 
 export const Message = (p:MessageType) => {
     return (
-        <div>{p.message}</div>
+        <div className={s.body}>
+            <div className={s.message}>{p.message}</div>
+            <div className={s.myMessage}>{p.message}</div>
+        </div>
+
     )
 }
